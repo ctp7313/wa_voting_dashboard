81ea081b18fa1efc5d571d3dac98ae24a7abfa65
wmt36 <wmt36@Williams-iMac.local>
2020-07-30 21:44:46 -0600

    updated the map to show locations of specific dropbox locations

--- ballots.js
+++ ballots.js
@@ -1,26 +1,55 @@
 $("#ballots").on("click", function() {
-    // var input = document.getElementById('addressTextBox').value;
-    // var storeInput = encodeURIComponent(input);
-    // localStorage.setItem("Address", storeInput);
+  
+  var map, infoWindow;
+function initMap() {
+    var normandy = { lat: 47.4243985, lng: -122.3369313}
+    var newPort = { lat: 47.5706684, lng: -122.1487738};   
+    var highPoint = { lat: 47.5481974, lng: -122.3756383};
+    var mercer = { lat: 47.5903476, lng: -122.2271448};
+    var kent = { lat: 47.4175067, lng: -122.1725965};
+    // The map, centered around washington
+     map = new google.maps.Map(
+        document.getElementById('map'), { zoom: 10, center: highPoint});
+    //putting markers at positions  polling locations
+    var marker = new google.maps.Marker({ position: newPort, map: map })
+    var marker = new google.maps.Marker({ position: kent, map: map })
+    var marker = new google.maps.Marker({ position: highPoint, map: map })
+    var marker = new google.maps.Marker({ position: mercer, map: map })
+    var marker = new google.maps.Marker({ position: normandy, map: map })
     
-    var address = localStorage.getItem("Address");
-    console.log(address);
-    var queryURL = "https://www.googleapis.com/civicinfo/v2/voterinfo?key=AIzaSyC2P1VzZTKxbNe2mCjAdBB6vyTqH9u9ZOo&address=" + address;
-  
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function(response) {
-  
-            var locations = response.dropOffLocations;
-            console.log(locations);
-            var codeAddress = JSON.stringify(locations[0].line1 + locations[0].city + locations[0].state + locations[0].zip);
-            var closeLoc = encodeURIComponent(locations[0]);
-            console.log(codeAddress);
-            
-            // for (var i = 0; i < 5; i++) {
-                
-            // }
 
-        })
-})
+    infoWindow = new google.maps.InfoWindow;
+
+    // code to integrate geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+        }, function () {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+}
+initMap();
+});
+
+
+
