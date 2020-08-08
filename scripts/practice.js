95d9e366999660c007a26f7a048e881f5b5971fc
Judy Naegeli <jnaegeli@gmail.com>
2020-08-08 10:01:40 -0700

    fixed a repeating problem in the officials code

--- scripts/practice.js
+++ scripts/practice.js
@@ -20,46 +20,52 @@ $("#candidates").on("click", function () {
 
         var officials = response.officials;
 
-        for (var i = 0; i < officials.length; i++) {
+        for (var i = 2; i < officials.length; i++) {
             var photo = officials[i].photoUrl;
             var name = officials[i].name;
             var party = officials[i].party;
-            var phone = officials[i].phones;
-            var email = officials[i].emails;
-            var website = officials[i].urls;
+            var phone = officials[i].phones || "phone number not provided";
+            var email = officials[i].emails || "email not provided";
+            var website = officials[i].urls || "website not provided";
             var office = sessionStorage.getItem(i);
 
+            var officialDiv = document.getElementById("offElem");
 
+            var cellDiv = $("<div>").attr("class", "cell");
+            $(officialDiv).append(cellDiv);
 
-            $("<div>", {
-                "class": "cell"
-            }).append([
-                $("<div>", {
-                    "class": "card"
-                }).append([
-                    $("<div>", {
-                        "class": "card-section",
-                        "id": "eachDiv"
-                    }),
-                    addInfo()
-                ])
-            ]).appendTo("#offElem")
+            var cardDiv = $("<div>").attr("class", "card");
+            $(cellDiv).append(cardDiv);
 
-            // function addInfo() {
-            //     var offArray = Object.keys(officials[i]);
-            //     console.log(offArray);
-            //     if (offArray.includes("photoUrl")) {
-            //         $("<img>", {
-            //             "class": "float-left",
-            //             "src": photo,
-            //             "style": "margin-right: 40px;",
-            //             "width": "200px",
-            //             "alt": "Portrait",
-            //             "id": "Portrait"
-            //         }).appendTo("#eachDiv")
-            //     }
-            // }
-        }
+            var sectionDiv = $("<div>").attr("class", "card-section");
+            $(cardDiv).append(sectionDiv);
 
-    })
-})
+            var portraitImg = $("<img>").attr({"class": "float-left", "style": "margin-right: 40px", "width": "200px", "alt": "Portrait", "id": "portrait"});
+            if (photo) {
+                $(portraitImg).attr("src", photo);
+                $(sectionDiv).append(portraitImg);
+            }
+
+            var offName = $("<h3>").attr("id", "official-name");
+            $(offName).text(name);
+            $(sectionDiv).append(offName);
+
+            var offOffice = $("<p>").attr({"id": "official-office", "style": "font-weight: bold"});
+            $(offOffice).text(office);
+            $(sectionDiv).append(offOffice);
+            
+            var offParty = $("<p>").attr("id", "official-party");
+            $(offParty).text(party);
+            $(sectionDiv).append(offParty);
+
+            var offContact = $("<p>").attr("id", "official-contact");
+            $(offContact).text("Contact Info: " + phone + " | " + email);
+            $(sectionDiv).append(offContact);
+
+            var offWebsite = $("<a>").attr({"id": "official-website", "target": "_blank"});
+            $(offWebsite).attr("href", website).text(website);
+            $(sectionDiv).append(offWebsite);
+
+        };
+    });
+});
